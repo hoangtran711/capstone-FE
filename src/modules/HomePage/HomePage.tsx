8a8c9e47ecef44317d13270f76d1073c64533de8
Tan Thanh <tanthanh@gmail.com>
2022-08-20 13:02:19 +0700

    Update landing page route

---
+++ src/modules/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Wrapper } from './HomePage.styled';
+
+const HomePage = () => {
+  return (
+    <Wrapper>
+      <h1 className="title">Tao la tran ne</h1>
+    </Wrapper>
+  );
+};
+
+export default HomePage;
